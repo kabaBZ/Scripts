79b3fa75a9d66e669bc8039140806ebc8852f7b0
kaba <1137088773@qq.com>
2023-11-03 20:55:45 +0800

    容器启动时先执行任务一次

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ schedule.every().day.at("10:00").do(aliCheck.run)
 
 schedule.every().day.at("14:00").do(CrawlIP.run)
 
+
+aliCheck.run()
+CrawlIP.run()
+
 while True:
     schedule.run_pending()
     time.sleep(1)
